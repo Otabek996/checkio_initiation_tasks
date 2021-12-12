a1fb50c853ffba10c2945cadaff56ec694d2fdc3
Otabek To'ychiyev <otabekotabek996996@gmail.com>
2021-12-12 19:58:29 +0500

    Solved task 6

---
+++ task_6.py
@@ -0,0 +1,12 @@
+# Task 6 - Backward String 
+
+# Solution 1
+def backward_string(val: str):
+    return val[::-1]
+
+# Solution 1
+backward_string = lambda val: val[::-1]
+
+
+print(backward_string('val'))
+print(backward_string(''))
